4d4d8bcd7a06d57ce5b11fc218497d706c326972
Amelia Ikeda <amelia@r-a-d.io>
2015-12-29 15:39:46 +0000

    Update the user model factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -13,9 +13,10 @@
 
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
-        'name' => $faker->name,
+        'username' => $faker->userName,
         'email' => $faker->email,
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
+        'dj_id' => null,
     ];
 });
