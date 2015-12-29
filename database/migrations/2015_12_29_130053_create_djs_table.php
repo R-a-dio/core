2f59bc4aba409c9a5ddbd5c9de4fe3a56354cfc8
Amelia Ikeda <amelia@r-a-d.io>
2015-12-29 21:37:19 +0000

    Make dj columns nullable/default

--- database/migrations/2015_12_29_130053_create_djs_table.php
+++ database/migrations/2015_12_29_130053_create_djs_table.php
@@ -16,8 +16,8 @@ class CreateDjsTable extends Migration
             $table->increments('id');
             $table->integer('theme_id')->nullable();
             $table->string('djname');
-            $table->string('djimage');
-            $table->boolean('visible');
+            $table->string('djimage')->nullable();
+            $table->boolean('visible')->default(false);
             $table->integer('priority');
 
             $table->timestamps();
