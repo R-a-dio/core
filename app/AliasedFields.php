c4558ac1f1a45c9ecc12de8282329c2148959799
Amelia Ikeda <amelia@r-a-d.io>
2015-12-29 15:21:26 +0000

    Make aliased fields affect the json output of a model

--- app/AliasedFields.php
+++ app/AliasedFields.php
@@ -4,6 +4,11 @@ namespace App;
 
 trait AliasedFields
 {
+  /**
+   * @param $key
+   *
+   * @return mixed
+   */
   public function getAttribute($key)
   {
     if (array_key_exists($key, $this->aliases)) {
@@ -13,6 +18,12 @@ trait AliasedFields
     return parent::getAttribute($key);
   }
 
+  /**
+   * @param $key
+   * @param $value
+   *
+   * @return mixed
+   */
   public function setAttribute($key, $value)
   {
     if (array_key_exists($key, $this->aliases)) {
@@ -20,5 +31,23 @@ trait AliasedFields
     }
 
     return parent::setAttribute($key, $value);
+  }
+
+  /**
+   * Show aliased output for arrays.
+   *
+   * @return array
+   */
+  public function toArray()
+  {
+    $array = parent::toArray();
+    $replacements = array_flip($this->aliases);
+    list($keys, $values) = array_divide($array);
+
+    $keys = array_map(function ($key) use ($replacements) {
+      return array_key_exists($key, $replacements) ? $replacements[$key] : $key;
+    }, $keys);
+
+    return array_combine($keys, $values);
   }
 }
