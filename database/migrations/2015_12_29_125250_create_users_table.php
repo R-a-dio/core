bc66bf0537bc7ed73008ff62f6f628318a1e9dcb
Amelia Ikeda <amelia@r-a-d.io>
2015-12-29 14:28:14 +0000

    Replace the users migration with our own one

--- database/migrations/2015_12_29_125250_create_users_table.php
+++ database/migrations/2015_12_29_125250_create_users_table.php
@@ -14,9 +14,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('user')->unique();
             $table->string('email')->unique();
-            $table->string('password', 60);
+            $table->string('pass');
+            $table->integer('djid')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
