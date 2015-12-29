880804d6d694c3c4395374e8b16f5dfa4967de5c
Amelia Ikeda <amelia@r-a-d.io>
2015-12-29 15:19:45 +0000

    Alias pass/user on the User object and add a DJ relation

--- app/User.php
+++ app/User.php
@@ -6,13 +6,15 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
+    use AliasedFields;
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'username', 'email', 'password',
     ];
 
     /**
@@ -23,4 +25,29 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * alias => original.
+     *
+     * @var array
+     */
+    protected $aliases = [
+        'password' => 'pass',
+        'username' => 'user',
+        'dj_id' => 'djid',
+    ];
+
+    /**
+     * The types we should cast attributes to
+     *
+     * @var array
+     */
+    protected $casts = [
+        'admin' => 'boolean',
+    ];
+
+    public function dj()
+    {
+        return $this->belongsTo(DJ::class);
+    }
 }
