833996e49523b9bf5e0c57b2bfe89b515e15216d
Amelia Ikeda <amelia@r-a-d.io>
2015-12-30 18:50:56 +0000

    Make the User class extend our own model

--- app/User.php
+++ app/User.php
@@ -3,11 +3,19 @@
 namespace App;
 
 use App\Traits\AliasedFields;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-class User extends Authenticatable
+class User extends Model implements
+    AuthenticatableContract,
+    AuthorizableContract,
+    CanResetPasswordContract
 {
-    use AliasedFields;
+    use Authenticatable, Authorizable, CanResetPassword, AliasedFields;
 
     /**
      * The attributes that are mass assignable.
