c2bd4f4d2fb7a41e8aa62bb84b693bbf9c0d22d9
Amelia Ikeda <amelia@r-a-d.io>
2016-01-03 11:02:15 +0000

    Set up icecast, elasticsearch and ezstream

--- after.sh
+++ after.sh
@@ -2,22 +2,33 @@
 
 DIR="/home/vagrant/radio"
 
+# set up icecast
+wget -qO - http://icecast.org/multimedia-obs.key | sudo apt-key add -
+echo "deb http://download.opensuse.org/repositories/multimedia:/xiph/xUbuntu_14.04/ ./" >> /etc/apt/sources.list.d/icecast.list
+
+# set up elasticsearch
+wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" >> /etc/apt/sources.list.d/elasticsearch-2.x.list
+
 # add dependencies for python packages
-apt-get install -y python-pip python-dev lame flac mpg123
+apt-get update
+apt-get install -y ezstream elasticsearch icecast2 python-dev lame flac mpg123 libmysqlclient-dev libshout3-dev
 
 # upgrade pip because ouch
 curl -sS https://bootstrap.pypa.io/get-pip.py | python
 pip install -U virtualenv ndg-httpsclient
 
-function install() {
+function install-virtualenv() {
   local virtualenv=$1
-  cd "${DIR}/virtualenvs/${virtualenv}"
+  cd "${DIR}/components/${virtualenv}"
   virtualenv .
   source bin/activate
   python setup.py install
   deactivate
 }
 
-# go through each virtualenv and set them up
-install verifier
-#install hanyuu
+install-virtualenv verifier
+# install-virtualenv hanyuu
+
+
+
