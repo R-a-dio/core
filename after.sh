4778a620330c5ef3c1273f6c1acba8294debad3a
Amelia Ikeda <amelia@r-a-d.io>
2016-01-04 21:59:33 +0000

    Make icecast start properly when installing

--- after.sh
+++ after.sh
@@ -14,6 +14,11 @@ echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" >> /e
 apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get install -y ezstream elasticsearch icecast2 python-dev lame flac mpg123 libmysqlclient-dev libshout3-dev libffi-dev
 
+cp "${DIR}/components/icecast/config.xml" /etc/icecast2/icecast.xml
+cp "${DIR}/components/icecast/icecast2.defaults" /etc/default/icecast2
+
+service icecast2 start
+
 # upgrade pip because ouch
 curl -sS https://bootstrap.pypa.io/get-pip.py | python
 pip install -U ndg-httpsclient virtualenv
